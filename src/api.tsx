99eee88f5c0ac28b07be9a4b01736f549416a351
urana123 <uranhand60@gmail.com>
2022-09-16 10:33:13 +0800

    done

--- src/api.tsx
+++ src/api.tsx
@@ -5,18 +5,18 @@ interface Props {
   data: String;
 }
 export const sendRequest = async ({ url, method, data }: Props) => {
-  console.log(process.env.REACT_APP_APP_ID);
-  const request_url = process.env.REACT_APP_BASE_URL + url;
+  console.log(process.env.DUMMY_BASE_URL);
+  const request_url = process.env.DUMMY_BASE_URL + url;
 
-  const response = await axios({
-    url: request_url,
-    method,
-    headers: {
-      "app-id": process.env.REACT_APP_APP_ID,
-      "Content-type": "application/json",
-    },
-    data,
-  });
+//   const response = await axios({
+//     url: request_url,
+//     method,
+//     headers: {
+//       "app-id": process.env.REACT_APP_APP_ID,
+//       "Content-type": "application/json",
+//     },
+//     data,
+//   });
 
-  console.log(response);
+//   console.log(response);
 };
