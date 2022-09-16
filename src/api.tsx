aeda290d2d3e417b06be10e2186406d66cef5b9f
urana123 <uranhand60@gmail.com>
2022-09-16 10:29:57 +0800

    done two

---
+++ src/api.tsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+interface Props {
+  url: String;
+  method: String;
+  data: String;
+}
+export const sendRequest = async ({ url, method, data }: Props) => {
+  console.log(process.env.REACT_APP_APP_ID);
+  const request_url = process.env.REACT_APP_BASE_URL + url;
+
+  const response = await axios({
+    url: request_url,
+    method,
+    headers: {
+      "app-id": process.env.REACT_APP_APP_ID,
+      "Content-type": "application/json",
+    },
+    data,
+  });
+
+  console.log(response);
+};
